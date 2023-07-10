913378ad4305f45c5c42c35c85aacc81292d82d2
archiesevillano <archie.sevillano29@gmail.com>
2023-07-10 23:02:51 +0800

    Removed Download.js file

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const axios = require('axios');
 const app = express();
 const PORT = 3001 || process.env.PORT;
 const converter = require("./endpoints/Converter");
-const download = require("./endpoints/Download");
 
 app.use(express.json());
 app.use(cors({
@@ -13,7 +12,6 @@ app.use(cors({
 }));
 
 app.use("/api", converter);
-app.use("/api", download);
 
 app.listen(PORT, () => {
     console.log("Enplay Server is now running");
